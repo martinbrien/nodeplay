41ad67abec49da0896e1420b44ac1e58267286c2
Martin Brien <martin@martinbrien.com>
2019-06-20 10:59:56 +0000

    added something

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-time ansible-playbook -i mbrien, --vault-password-file=/home/martin/.pwd nodeplay.yml -k --extra-vars "hostname=mbrien username=martin archive=tarsnap"   
-
+time ansible-playbook -i mbrien, \
+--vault-password-file=/home/martin/.pwd \
+nodeplay.yml \
+-k \
+--extra-vars "hostname=mbrien username=martin archive=tarsnap" \
+#--tags "namesilo3" 
